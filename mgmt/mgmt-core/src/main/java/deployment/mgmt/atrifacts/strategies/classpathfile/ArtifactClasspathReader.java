ad49b274878c198b1fc19b376391a860e8f7d7e4
Selyumin <Selyumin.V.Al@sberbank.ru>
2019-12-05 20:32:13 +0300

    Added another artifact resolving strategy.

---
+++ mgmt/mgmt-core/src/main/java/deployment/mgmt/atrifacts/strategies/classpathfile/ArtifactClasspathReader.java
@@ -0,0 +1,36 @@
+package deployment.mgmt.atrifacts.strategies.classpathfile;
+
+import deployment.mgmt.atrifacts.Artifact;
+import java.io.File;
+import java.util.List;
+import java.util.Objects;
+
+import static deployment.mgmt.utils.ZipUtils.readInnerFile;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toList;
+
+public class ArtifactClasspathReader implements JarClasspathReader {
+    private static final int MINIMAL_IDENTIFIERS_COUNT = 3;
+
+    @Override
+    public List<Artifact> extractClasspath(File artifactFile, Artifact artifact) {
+        String classpath = new String(readInnerFile(artifactFile, CLASSPATH_GRADLE_FILE));
+
+        return stream(classpath.split(", "))
+                .map(ArtifactClasspathReader::toArtifact)
+                .filter(Objects::nonNull)
+                .collect(toList());
+    }
+
+    private static Artifact toArtifact(String mavenString) {
+        if (hasEnoughData(mavenString)) {
+            return Artifact.fromMavenString(mavenString);
+        } else {
+            return null;
+        }
+    }
+
+    private static boolean hasEnoughData(String mavenString) {
+        return mavenString.chars().filter(ch -> ch == ':').count() >= MINIMAL_IDENTIFIERS_COUNT;
+    }
+}
